ea3c05aa9e346a44d03823f816c11ea7b78a757d
blubrano <blubrano@chicagobooth.edu>
2021-08-15 20:57:24 +0000

    RCAV v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,35 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end 
+  
+  
+  def play_rock
+    #write a ton of code to do whatever
+
+    # redirect_to("https://www.wikipedia.org")
+
+    # render({ :html => "<h1>Hello, world!</h1>".html_safe })
+
+    render({ :template => "game_templates/user_rock.html.erb" })
+    
+  end
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render ({ :template => "game_templates/user_paper.html.erb" })
+  end
 end
